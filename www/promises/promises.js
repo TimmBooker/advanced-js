047dce7d5d8ecacbbbae5cf1208b258fbd74ef68
Ryan Morris <mr.morris@gmail.com>
2017-06-11 13:14:16 +0530

    Promises lab

---
+++ www/promises/promises.js
@@ -0,0 +1,27 @@
+/**
+ * Write a function
+ *   callLater(fn, ms);
+ * that expects two arguments, a function and time in milliseconds
+ *
+ * The function will set up the passed fn to be invoked after ms milliseconds
+ * It should return a promise that is fulfilled once fn function runs
+ * It should reject if the ms delay is 0 or less.   
+ * (tip: uses promises and setTimeout)
+ *
+ * Bonus:
+ * Write another function:
+ *   callLaterWaterfall(fns, ms);
+ * The function expects two arguments, an array of functions and time in ms
+ * Upon invokation it will run each function in sequence, with
+ * ms milliseconds delay between calls
+ * (tip: uses promises, async and await)
+ */
+
+function callLater(fn, ms) {
+
+	// implement me...
+
+}
+
+// test me here...
+// callLater();
