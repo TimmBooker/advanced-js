3a6080d05a4f7a863cbf5b877d486492797c98ae
Ryan Morris <mr.morris@gmail.com>
2017-06-12 05:41:21 +0530

    promises solution

--- www/promises/promises.js
+++ www/promises/promises.js
@@ -19,9 +19,37 @@
 
 function callLater(fn, ms) {
 
-	// implement me...
+	return new Promise(function(resolve, reject) {
+		setTimeout(function() {
+			fn();
+			resolve(1);
+		}, ms);
+	});
 
 }
 
 // test me here...
-// callLater();
+var prom = callLaterWaterfall([function() {
+	console.log('hi');
+}, function(){console.log('poo')}, function(){console.log('three')}], 1000).then(function(data) {
+	console.log(data);
+});
+
+// bonus:
+async function callLaterWaterfall(fns, ms) {
+
+	var promHolder;
+
+	for (let i=0; i<fns.length; i++) {
+		promHolder = await new Promise(function(resolve, reject) {
+			setTimeout(function() {
+				fns[i]();
+				resolve(1);
+			}, ms);
+		});
+	};
+
+	return promHolder;
+
+}
+
