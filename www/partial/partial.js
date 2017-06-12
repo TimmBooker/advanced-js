13c35091466d361e8362633f588850b428e22a7d
Ryan Morris <mr.morris@gmail.com>
2017-06-12 12:59:03 +0530

    Partial solution update

--- www/partial/partial.js
+++ www/partial/partial.js
@@ -5,7 +5,7 @@
     	magnitude: 10,
     	add: function (x, y) {
 
-			return (x + y) + this.magnitude;
+			return (x + y) * this.magnitude;
 
 		}
 
@@ -16,12 +16,25 @@
  */
 Function.prototype.partial = function() {
 
-  // get obj
+  // A simple Partial
+  // This gets the tests to pass, but
+  // - it supports only one "hard coded" argument
+  // - it creates a FULL copy of the function using bind()
   var args = Array.prototype.slice.call(arguments);
   var obj = args.shift();
-  // get other arguments
-
   return this.bind(obj, args[0]);
-  // create a new function from this function with bound context
+
+  // A better Partial
+  // - supports multiple arguments
+  // - creates a new function but not a FULL copy of the function using bind()
+  var args = Array.from(arguments);
+  var context = args.shift();
+  var func = this;
+
+  return function () {
+  	// arguments here is of sum10()
+    var newArgs = Array.from(arguments);
+    return func.apply(context, args.concat(newArgs));
+  };
 
 };
